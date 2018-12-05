856e814dda583ee119d0f28a329f3266b3d67db1
Jacobus Harding <jacobus.anthony.harding@gmail.com>
2018-12-05 21:34:12 +0100

    working for native test build, deployed PWA has strange behaviour. Popups (permissions?) on start and freezes when trying to scan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BLE } from '@ionic-native/ble/ngx';
+// import { Camera } from '@ionic-native/camera';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
