050d5f572d2eb084f0d870dd1a9b8ccde4fdea01
Jacobus Harding <jacobus.anthony.harding@gmail.com>
2018-12-06 14:11:12 +0100

    sigh

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,6 +19,7 @@ export class HomePage implements OnInit {
 
   triggerTemplate;
   isCollapsed = false;
+  deferredPrompt;
 
   constructor(private ble: BLE
               // , private alertCtrl: AlertController,
@@ -28,8 +29,29 @@ export class HomePage implements OnInit {
   }
   
   ngOnInit() {
-
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
   }
+
+  installPWA() {
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
+  }
+
+  
 
   displayMsg(msg: string) {
     this.ngZone.run(() => {
